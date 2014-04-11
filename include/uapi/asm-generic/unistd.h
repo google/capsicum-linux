1f5882061f359f3db3646caa4e7113d499bd6d16
David Drysdale <drysdale@google.com>
2014-04-11 10:48:55 +0100

    capsicum,x86: add syscalls to limit FD rights
    Add the cap_rights_get(2) and cap_rights_set(2) syscalls to
    allow retrieval and modification of the rights associated with
    a file descriptor.
    
    When a normal file descriptor has its rights restricted in any
    way, it becomes a Capsicum capability file descriptor.  This is
    a wrapper struct file that is installed in the fdtable in place
    of the original file.  From this point on, when the FD is converted
    to a struct file by fget() (or equivalent), the wrapper is checked
    for the appropriate rights and the wrapped inner normal file is
    returned.
    
    When a Capsicum capability file descriptor has its rights restricted
    further (they cannot be expanded), a new wrapper is created with
    the restricted rights, also wrapping the same inner normal file.
    In other words, the .underlying field in a struct capsicum_capability
    is always a normal file, never another Capsicum capability file.
    
    These syscalls specify the different components of the compound
    rights structure separately, allowing components to be unspecified
    for no change.
    
    Note that in FreeBSD 10.x the function of this pair of syscalls
    is implemented as 3 distinct pairs of syscalls, one pair for each
    component of the compound rights (primary/fcntl/ioctl).
    
    Signed-off-by: David Drysdale <drysdale@google.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -709,9 +709,13 @@ __SYSCALL(__NR_memfd_create, sys_memfd_create)
 __SYSCALL(__NR_bpf, sys_bpf)
 #define __NR_execveat 281
 __SC_COMP(__NR_execveat, sys_execveat, compat_sys_execveat)
+#define __NR_cap_rights_limit 282
+__SYSCALL(__NR_cap_rights_limit, sys_cap_rights_limit)
+#define __NR_cap_rights_limit 283
+__SYSCALL(__NR_cap_rights_get, sys_cap_rights_get)
 
 #undef __NR_syscalls
-#define __NR_syscalls 282
+#define __NR_syscalls 284
 
 /*
  * All syscalls below here should go away really,
