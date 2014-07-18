143aab7466bb69de70fc1e99729a35c53935ca84
David Drysdale <drysdale@google.com>
2014-07-18 17:25:01 +0100

    prctl: add flag to force use of O_BENEATH
    Add a per-task flag that indicates all openat(2) operations
    should implicitly have the O_BENEATH flag set.
    
    Add a prctl(2) command to set this flag. Include an option to
    force the flag set to be synchronized across all tasks in the
    thread group.
    
    Signed-off-by: David Drysdale <drysdale@google.com>

--- fs/namei.c
+++ fs/namei.c
@@ -2173,6 +2173,9 @@ static const char *path_init(struct nameidata *nd, unsigned flags)
 	nd->depth = 0;
 	nd->dfd_cap.file = NULL;
 	nd->dfd_cap.flags = 0;
+	if (task_openat_beneath(current))
+		nd->flags |= LOOKUP_BENEATH;
+
 	if (flags & LOOKUP_ROOT) {
 		struct dentry *root = nd->root.dentry;
 		struct inode *inode = root->d_inode;
@@ -2202,7 +2205,7 @@ static const char *path_init(struct nameidata *nd, unsigned flags)
 
 	nd->m_seq = read_seqbegin(&mount_lock);
 	if (*s == '/') {
-		if (flags & LOOKUP_BENEATH)
+		if (nd->flags & LOOKUP_BENEATH)
 			return ERR_PTR(-ENOTBENEATH);
 		if (flags & LOOKUP_RCU)
 			rcu_read_lock();
