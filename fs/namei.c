62b93c1d5004ab37c52ef7945e7cfebe04bfb7f3
David Drysdale <drysdale@google.com>
2014-07-18 17:25:01 +0100

    capsicum: prctl(2) to force use of O_BENEATH
    Add a per-task flag that indicates all openat(2) operations
    should implicitly have the O_BENEATH flag set.
    
    Add a prctl(2) command to set this flag (irrevocably). Include
    an option to force the flag set to be synchronized across all
    tasks in the thread group.
    
    Signed-off-by: David Drysdale <drysdale@google.com>

--- fs/namei.c
+++ fs/namei.c
@@ -1858,6 +1858,9 @@ static int path_init(int dfd, const char *name, unsigned int flags,
 	nd->flags = flags | LOOKUP_JUMPED;
 	nd->depth = 0;
 
+	if (task_openat_beneath(current))
+		nd->flags |= LOOKUP_BENEATH;
+
 	if (flags & LOOKUP_ROOT) {
 		struct dentry *root = nd->root.dentry;
 		struct inode *inode = root->d_inode;
@@ -1884,7 +1887,7 @@ static int path_init(int dfd, const char *name, unsigned int flags,
 
 	nd->m_seq = read_seqbegin(&mount_lock);
 	if (*name=='/') {
-		if (flags & LOOKUP_BENEATH)
+		if (nd->flags & LOOKUP_BENEATH)
 			return -EPERM;
 		if (dfd_rights)
 			*dfd_rights = NULL;
