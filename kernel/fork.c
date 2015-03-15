d0264fa02b32cc26474216b5660bfca2454b885f
Josh Triplett <josh@joshtriplett.org>
2015-03-15 01:00:20 -0700

    clone4: Add a CLONE_FD flag to get task exit notification via fd
    When passed CLONE_FD, clone4 hands the caller a file descriptor
    referring to the new process.  When the new process exits, the file
    descriptor becomes readable, producing a structure containing the exit
    status, exit code, and user/system times.  The file descriptor also
    works in epoll, poll, and select.
    
    This allows libraries to safely launch and manage child processes on
    behalf of a caller, without taking over or interfering with process-wide
    signal handling.  Without this, such a library would need to take over
    or cooperate with the entire process's SIGCHLD handling, either via a
    signal handler or a signalfd.
    
    Signed-off-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -89,6 +89,8 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/task.h>
 
+#include "clonefd.h"
+
 /*
  * Minimum number of threads to boot the kernel
  */
@@ -1281,7 +1283,8 @@ static struct task_struct *copy_process(u64 clone_flags,
 					struct clone4_args *args,
 					struct pid *pid,
 					int trace,
-					int node)
+					int node,
+					struct clonefd_setup *clonefd_setup)
 {
 	int retval;
 	struct task_struct *p;
@@ -1502,6 +1505,10 @@ static struct task_struct *copy_process(u64 clone_flags,
 		}
 	}
 
+	retval = clonefd_do_clone(clone_flags, p, args, clonefd_setup);
+	if (retval)
+		goto bad_fork_free_pid;
+
 	p->set_child_tid = (clone_flags & CLONE_CHILD_SETTID) ? args->ctid : NULL;
 	/*
 	 * Clear TID on mm_release()?
@@ -1568,7 +1575,7 @@ static struct task_struct *copy_process(u64 clone_flags,
 	 */
 	retval = cgroup_can_fork(p);
 	if (retval)
-		goto bad_fork_free_pid;
+		goto bad_fork_cleanup_clonefd;
 
 	/*
 	 * Make it visible to the rest of the system, but dont wake it up yet.
@@ -1659,6 +1666,8 @@ static struct task_struct *copy_process(u64 clone_flags,
 
 bad_fork_cancel_cgroup:
 	cgroup_cancel_fork(p);
+bad_fork_cleanup_clonefd:
+	clonefd_cleanup_failed_clone(clonefd_setup);
 bad_fork_free_pid:
 	if (pid != &init_struct_pid)
 		free_pid(pid);
@@ -1718,7 +1727,7 @@ struct task_struct *fork_idle(int cpu)
 	struct task_struct *task;
 	struct clone4_args args = {};
 	task = copy_process(CLONE_VM, &args, &init_struct_pid, 0,
-                            cpu_to_node(cpu));
+			cpu_to_node(cpu), NULL);
 	if (!IS_ERR(task)) {
 		init_idle_pids(task->pids);
 		init_idle(task, cpu);
@@ -1738,6 +1747,7 @@ long _do_fork(u64 clone_flags, struct clone4_args *args)
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+	struct clonefd_setup clonefd_setup = {};
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
@@ -1757,7 +1767,8 @@ long _do_fork(u64 clone_flags, struct clone4_args *args)
 			trace = 0;
 	}
 
-	p = copy_process(clone_flags, args, NULL, trace, NUMA_NO_NODE);
+	p = copy_process(clone_flags, args, NULL, trace, NUMA_NO_NODE,
+			 &clonefd_setup);
 	/*
 	 * Do this prior waking up the new thread - the thread pointer
 	 * might get invalid after that point, if the thread exits quickly.
@@ -1779,6 +1790,8 @@ long _do_fork(u64 clone_flags, struct clone4_args *args)
 			init_completion(&vfork);
 			get_task_struct(p);
 		}
+
+		clonefd_install_fd(args, &clonefd_setup);
 
 		wake_up_new_task(p);
 
@@ -1923,6 +1936,8 @@ COMPAT_SYSCALL_DEFINE4(clone4, unsigned, flags_high, unsigned, flags_low,
 	kargs.stack_start = compat_kargs.stack_start;
 	kargs.stack_size = compat_kargs.stack_size;
 	kargs.tls = compat_kargs.tls;
+	kargs.clonefd = compat_ptr(compat_kargs.clonefd);
+	kargs.clonefd_flags = compat_kargs.clonefd_flags;
 	return _do_fork(flags, &kargs);
 }
 #endif /* CONFIG_COMPAT */
