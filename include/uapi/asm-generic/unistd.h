a76699227102a70a6f9270a001e0f53b0f74650b
Josh Triplett <josh@joshtriplett.org>
2015-03-15 00:59:47 -0700

    Introduce a new clone4 syscall with more flag bits and extensible arguments
    clone() has no more usable flags available.  It has three now-unused
    flags (CLONE_PID, CLONE_DETACHED, and CLONE_STOPPED), but current
    kernels just ignore those flags without returning an error like EINVAL,
    so reusing those flags would not allow userspace to detect the
    availability of the new functionality.
    
    Introduce a new system call, clone4, which accepts a second 32-bit flags
    field.  clone4 also returns EINVAL for the currently unused flags in
    clone, allowing their reuse.
    
    To process these new flags, change the flags argument of _do_fork to a
    u64.  sys_clone and do_fork both still use "unsigned long" for flags as
    they did before, truncating it to 32-bit and masking out the obsolete
    flags to behave like clone currently does.
    
    clone4 accepts its remaining arguments as a structure, and userspace
    passes in the size of that structure.  clone4 has well-defined semantics
    that allow extending that structure in the future.  New userspace
    passing in a larger structure than the kernel expects will receive
    EINVAL, and can use a smaller structure to work with old kernels.  New
    kernels accept smaller argument structures passed by userspace, and any
    un-passed arguments default to 0.
    
    clone4 handles arguments in the same order on all architectures, with no
    backwards variations; to do so, it depends on the new
    HAVE_COPY_THREAD_TLS.
    
    The new system call currently accepts exactly the same flags as clone;
    future commits will introduce new flags for additional functionality.
    
    Signed-off-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -715,9 +715,11 @@ __SYSCALL(__NR_userfaultfd, sys_userfaultfd)
 __SYSCALL(__NR_membarrier, sys_membarrier)
 #define __NR_mlock2 284
 __SYSCALL(__NR_mlock2, sys_mlock2)
+#define __NR_clone4 287
+__SC_COMP(__NR_clone4, sys_clone4, compat_sys_clone4)
 
 #undef __NR_syscalls
-#define __NR_syscalls 285
+#define __NR_syscalls 288
 
 /*
  * All syscalls below here should go away really,
