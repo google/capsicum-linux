a864f45c9a268b2295c6aa903f60c40734d08b3b
David Drysdale <drysdale@google.com>
2015-03-27 13:10:44 +0000

    procdesc: add compat_ wrapper for pdwait4

--- fs/procdesc.c
+++ fs/procdesc.c
@@ -10,6 +10,7 @@
  */
 #include <linux/anon_inodes.h>
 #include <linux/audit.h>
+#include <linux/compat.h>
 #include <linux/fs.h>
 #include <linux/fdtable.h>
 #include <linux/file.h>
@@ -215,6 +216,36 @@ SYSCALL_DEFINE4(pdwait4, int, fd, int __user *, status, int, options,
 	if (pid == 0)
 		return -ECHILD;
 	return sys_wait4(pid, status, options, rusage);
+}
+
+COMPAT_SYSCALL_DEFINE4(pdwait4, int, fd,
+		compat_int_t __user *, stat_addr,
+		int, options,
+		struct compat_rusage __user *, ru)
+{
+	if (!ru) {
+		return sys_pdwait4(fd, stat_addr, options, NULL);
+	} else {
+		struct rusage r;
+		int ret;
+		unsigned int status;
+		mm_segment_t old_fs = get_fs();
+
+		set_fs (KERNEL_DS);
+		ret = sys_pdwait4(fd,
+				  (stat_addr ?
+				   (unsigned int __user *) &status : NULL),
+				  options, (struct rusage __user *) &r);
+		set_fs (old_fs);
+
+		if (ret > 0) {
+			if (put_compat_rusage(&r, ru))
+				return -EFAULT;
+			if (stat_addr && put_user(status, stat_addr))
+				return -EFAULT;
+		}
+		return ret;
+	}
 }
 
 /*
