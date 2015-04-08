6e335a23ec636af944c2ec4bf4251bb1540bb0e5
David Drysdale <drysdale@google.com>
2015-04-08 10:29:20 +0100

    clone4: minor tidy-up
    Add system call prototype and fix stack_start/size mismatch.
    Also fix checkpatch warnings.
    
    Signed-off-by: David Drysdale <drysdale@google.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1452,7 +1452,8 @@ static struct task_struct *copy_process(u64 clone_flags,
 	retval = copy_io(clone_flags, p);
 	if (retval)
 		goto bad_fork_cleanup_namespaces;
-	retval = copy_thread_tls(clone_flags, args->stack_start, args->stack_size, p, args->tls);
+	retval = copy_thread_tls(clone_flags, args->stack_start,
+				 args->stack_size, p, args->tls);
 	if (retval)
 		goto bad_fork_cleanup_io;
 
@@ -1468,11 +1469,13 @@ static struct task_struct *copy_process(u64 clone_flags,
 	if (retval)
 		goto bad_fork_free_pid;
 
-	p->set_child_tid = (clone_flags & CLONE_CHILD_SETTID) ? args->ctid : NULL;
+	p->set_child_tid = (clone_flags & CLONE_CHILD_SETTID) ?
+			    args->ctid : NULL;
 	/*
 	 * Clear TID on mm_release()?
 	 */
-	p->clear_child_tid = (clone_flags & CLONE_CHILD_CLEARTID) ? args->ctid : NULL;
+	p->clear_child_tid = (clone_flags & CLONE_CHILD_CLEARTID) ?
+			      args->ctid : NULL;
 #ifdef CONFIG_BLOCK
 	p->plug = NULL;
 #endif
@@ -1673,6 +1676,7 @@ struct task_struct *fork_idle(int cpu)
 {
 	struct task_struct *task;
 	struct clone4_args args = {};
+
 	task = copy_process(CLONE_VM, &args, &init_struct_pid, 0, NULL);
 	if (!IS_ERR(task)) {
 		init_idle_pids(task->pids);
@@ -1778,8 +1782,9 @@ long do_fork(unsigned long clone_flags,
 		.ptid = parent_tidptr,
 		.ctid = child_tidptr,
 		.stack_start = stack_start,
-		.stack_start = stack_size,
+		.stack_size = stack_size,
 	};
+
 	return _do_fork(squelch_clone_flags(clone_flags), &kargs);
 }
 #endif
@@ -1793,6 +1798,7 @@ pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags)
 		.stack_start = (unsigned long)fn,
 		.stack_size = (unsigned long)arg,
 	};
+
 	return _do_fork(flags|CLONE_VM|CLONE_UNTRACED, &kargs);
 }
 
@@ -1801,6 +1807,7 @@ SYSCALL_DEFINE0(fork)
 {
 #ifdef CONFIG_MMU
 	struct clone4_args kargs = {};
+
 	return _do_fork(SIGCHLD, &kargs);
 #else
 	/* can not support in nommu mode */
@@ -1813,6 +1820,7 @@ SYSCALL_DEFINE0(fork)
 SYSCALL_DEFINE0(vfork)
 {
 	struct clone4_args kargs = {};
+
 	return _do_fork(CLONE_VFORK | CLONE_VM | SIGCHLD, &kargs);
 }
 #endif
@@ -1847,6 +1855,7 @@ SYSCALL_DEFINE5(clone, unsigned long, clone_flags, unsigned long, newsp,
 		.stack_start = newsp,
 		.tls = tls,
 	};
+
 	return _do_fork(squelch_clone_flags(clone_flags), &kargs);
 }
 #endif
@@ -1857,6 +1866,7 @@ SYSCALL_DEFINE4(clone4, unsigned, flags_high, unsigned, flags_low,
 {
 	u64 flags = (u64)flags_high << 32 | flags_low;
 	struct clone4_args kargs = {};
+
 	if (args_size > sizeof(kargs))
 		return -EINVAL;
 	if (args_size && copy_from_user(&kargs, args, args_size))
@@ -1872,6 +1882,7 @@ COMPAT_SYSCALL_DEFINE4(clone4, unsigned, flags_high, unsigned, flags_low,
 	u64 flags = (u64)flags_high << 32 | flags_low;
 	struct compat_clone4_args compat_kargs = {};
 	struct clone4_args kargs = {};
+
 	if (args_size > sizeof(compat_kargs))
 		return -EINVAL;
 	if (args_size && copy_from_user(&compat_kargs, args, args_size))
