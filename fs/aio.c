5e1a6e3128654a3786363fad9263202a53e989d6
David Drysdale <drysdale@google.com>
2015-11-16 17:35:55 +0000

    capsicum, aio: use pre-computed rights table
    Rather than building a rights structure for each aio operation,
    pre-build a table of the possible rights needed, making use of
    the fact that the IOCB_CMD_* constants are a small, low-numbered
    set.
    
    Suggested-by: Wedson Almeida Filho <wedsonaf@google.com>

--- fs/aio.c
+++ fs/aio.c
@@ -266,6 +266,37 @@ static int __init aio_setup(void)
 }
 __initcall(aio_setup);
 
+
+/*
+ * aio operations on Capsicum capability file descriptors requires rights
+ * that are specific to the operation being performed. Set up such rights
+ * once and for all, relying on the IOCB_CMD_* constants being low-numbered.
+ */
+static struct capsicum_rights aio_rights[IOCB_CMD_PWRITEV + 1];
+static int __init init_aio_rights(void)
+{
+	int ii;
+
+	for (ii = 0; ii <= IOCB_CMD_PWRITEV; ii++)
+		cap_rights_init(&aio_rights[ii], CAP_PREAD, CAP_PWRITE,
+				CAP_POLL_EVENT, CAP_FSYNC);
+	cap_rights_init(&aio_rights[IOCB_CMD_PREAD], CAP_PREAD);
+	cap_rights_init(&aio_rights[IOCB_CMD_PREADV], CAP_PREAD);
+	cap_rights_init(&aio_rights[IOCB_CMD_PWRITE], CAP_PWRITE);
+	cap_rights_init(&aio_rights[IOCB_CMD_PWRITEV], CAP_PWRITE);
+	cap_rights_init(&aio_rights[IOCB_CMD_FSYNC], CAP_FSYNC);
+	cap_rights_init(&aio_rights[IOCB_CMD_FDSYNC], CAP_FSYNC);
+	return 0;
+}
+fs_initcall(init_aio_rights);
+
+static inline const struct capsicum_rights *aio_opcode_rights(int opcode)
+{
+	int index = (opcode <= IOCB_CMD_PWRITEV) ? opcode : IOCB_CMD_NOOP;
+
+	return &aio_rights[index];
+}
+
 static void put_aio_ring_file(struct kioctx *ctx)
 {
 	struct file *aio_ring_file = ctx->aio_ring_file;
@@ -1493,38 +1524,10 @@ rw_common:
 	return 0;
 }
 
-static struct capsicum_rights *
-aio_opcode_rights(struct capsicum_rights *rights, int opcode)
-{
-	switch (opcode) {
-	case IOCB_CMD_PREAD:
-	case IOCB_CMD_PREADV:
-		cap_rights_init(rights, CAP_PREAD);
-		break;
-
-	case IOCB_CMD_PWRITE:
-	case IOCB_CMD_PWRITEV:
-		cap_rights_init(rights, CAP_PWRITE);
-		break;
-
-	case IOCB_CMD_FSYNC:
-	case IOCB_CMD_FDSYNC:
-		cap_rights_init(rights, CAP_FSYNC);
-		break;
-
-	default:
-		cap_rights_init(rights, CAP_PREAD, CAP_PWRITE, CAP_POLL_EVENT,
-				CAP_FSYNC);
-		break;
-	}
-	return rights;
-}
-
 static int io_submit_one(struct kioctx *ctx, struct iocb __user *user_iocb,
 			 struct iocb *iocb, bool compat)
 {
 	struct aio_kiocb *req;
-	struct capsicum_rights rights;
 	ssize_t ret;
 
 	/* enforce forwards compatibility on users */
@@ -1548,8 +1551,7 @@ static int io_submit_one(struct kioctx *ctx, struct iocb __user *user_iocb,
 		return -EAGAIN;
 
 	req->common.ki_filp = fget_rights(iocb->aio_fildes,
-					  aio_opcode_rights(&rights,
-							iocb->aio_lio_opcode));
+				      aio_opcode_rights(iocb->aio_lio_opcode));
 	if (unlikely(IS_ERR(req->common.ki_filp))) {
 		ret = PTR_ERR(req->common.ki_filp);
 		req->common.ki_filp = NULL;
